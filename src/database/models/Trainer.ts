11b167d374dbf77bdfc6cef048a0fd9cb27cdf32
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-04-07 19:32:52 -0300

    Limite para o numero de pokemons que um treinador pode ter estabelecido.

--- src/database/models/Trainer.ts
+++ src/database/models/Trainer.ts
@@ -1,13 +1,20 @@
 import { model, Schema, Model } from 'mongoose';
 import {ITrainer} from './ITrainer';
 
+function arraySize(arr:Array<object>):boolean{
+    return arr.length<=7;
+}
+
 const pokemon:object = {
     type: Schema.Types.ObjectId,
     ref: 'Pokemon',
 }
 const TrainerSchema:Schema = new Schema({
     name: String,
-    pokemons: [pokemon]
+    pokemons:{
+        type:[pokemon],
+        validate: [arraySize, "Treinadores so podem ter no maximo 7 pokemons"]
+    } 
 });
 
 const Trainer: Model<ITrainer> = model('Trainer',TrainerSchema);
